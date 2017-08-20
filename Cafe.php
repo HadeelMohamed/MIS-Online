a58ee5cc3717879989c3cf77af1bc1644e34a1bf
Hadeel Mohammed <hadeel.mostafa.cs@gmail.com>
2017-08-20 10:23:55 +0200

    comment

---
+++ Cafe.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cafe extends Model
+{
+    //
+        public $table="cafes";
+     //  protected $connection = 'mysql2';
+}
